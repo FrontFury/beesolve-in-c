d219c7071fc0713ed840ddb3cb54e7dfd759e76b
estiakahamed01 <tasin220116@diit.edu.bd>
2026-05-30 20:33:55 +0600

    Added bee1007

---
+++ bee1007.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, C, D, DIF;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+
+    DIF = (A * B) - (C * D);
+
+    printf("DIFERENCA = %d\n", DIF);
+
+    return 0;
+}
