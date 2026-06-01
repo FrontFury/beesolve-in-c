f5f9783ce6d5f20be1782447801d8695f557d55d
estiakahamed01 <tasin220116@diit.edu.bd>
2026-06-01 16:23:13 +0600

    Print Odd Numbers (1–20)

---
+++ Practice/while2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    printf("Odd Numbers 1-20\n");
+
+    while (i <= 20) {
+        printf("%d\n", i);
+        i += 2;
+    }
+
+    return 0;
+}
