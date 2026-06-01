10021b6bb00f5e00d26580cbc6860a31b95db301
estiakahamed01 <tasin220116@diit.edu.bd>
2026-06-01 16:23:55 +0600

    Print Even Numbers (1–20)

---
+++ Practice/while3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i = 2;
+
+    printf("Even Numbers 1-20\n");
+
+    while (i <= 20) {
+        printf("%d\n", i);
+        i += 2;
+    }
+
+    return 0;
+}
