6104d6b9afa85d2a81d5bcb4229ffa729e3cc3c4
estiakahamed01 <tasin220116@diit.edu.bd>
2026-06-02 18:53:14 +0600

    Checking Capital & small letter

---
+++ Practice/capitalOrSmall.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("Capital Letter\n");
+    } else if (ch >= 'a' && ch <= 'z') {
+        printf("Small Letter\n");
+    } else {
+        printf("Invalid Input\n");
+    }
+
+    return 0;
+}
