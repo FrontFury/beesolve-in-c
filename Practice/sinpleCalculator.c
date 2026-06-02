61229ad938a29a06036be11e32ccaa2913788021
estiakahamed01 <tasin220116@diit.edu.bd>
2026-06-02 19:00:08 +0600

    Added Simple Calculator

---
+++ Practice/sinpleCalculator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    double num1, num2, result;
+
+    printf("=== Simple Calculator ===\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+
+    printf("Choose an operation (1-4): ");
+    scanf("%d", &choice);
+
+    if (choice <= 0 || choice >= 5) {
+        printf("Invalid choice!\n");
+    } else {
+        printf("Enter first number: ");
+        scanf("%lf", &num1);
+
+        printf("Enter second number: ");
+        scanf("%lf", &num2);
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                printf("Result = %.2lf\n", result);
+                break;
+
+            case 2:
+                result = num1 - num2;
+                printf("Result = %.2lf\n", result);
+                break;
+
+            case 3:
+                result = num1 * num2;
+                printf("Result = %.2lf\n", result);
+                break;
+
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    printf("Result = %.2lf\n", result);
+                } else {
+                    printf("Cannot divide by zero!\n");
+                }
+                break;
+
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+
+    return 0;
+}
